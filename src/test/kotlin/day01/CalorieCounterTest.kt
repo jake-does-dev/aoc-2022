52d503de7c3b876fd636aa4574ff61613231da6d
jake-does-dev <jjemail1995@gmail.com>
2022-12-02 11:28:36 +0000

    day01

--- src/test/kotlin/day01/CalorieCounterTest.kt
+++ src/test/kotlin/day01/CalorieCounterTest.kt
@@ -6,21 +6,21 @@ import kotlin.test.assertEquals
 internal class CalorieCounterTest {
     @Test
     fun maxCaloriesExample() {
-        assertEquals(24000, sumTopNCalories("src/main/resources/day01-example.txt"))
+        assertEquals(24000, sumTopNCalories("src/main/resources/day01/day01-example.txt"))
     }
 
     @Test
     fun maxCaloriesPuzzle() {
-        assertEquals(69289, sumTopNCalories("src/main/resources/day01-puzzle.txt"))
+        assertEquals(69289, sumTopNCalories("src/main/resources/day01/day01-puzzle.txt"))
     }
 
     @Test
     fun topNCaloriesSumExample() {
-        assertEquals(45000, sumTopNCalories("src/main/resources/day01-example.txt", topN = 3))
+        assertEquals(45000, sumTopNCalories("src/main/resources/day01/day01-example.txt", topN = 3))
     }
 
     @Test
     fun topNCaloriesSumPuzzle() {
-        assertEquals(205615, sumTopNCalories("src/main/resources/day01-puzzle.txt", topN = 3))
+        assertEquals(205615, sumTopNCalories("src/main/resources/day01/day01-puzzle.txt", topN = 3))
     }
 }
