744ed65ed7fc0f27a5dc4f88bdce3c374e792914
Jake Allan <jjemail1995@gmail.com>
2022-12-03 11:44:51 +0000

    day01 refactor

---
+++ src/test/kotlin/day01/CalorieCounterKtTest.kt
@@ -0,0 +1,26 @@
+package day01
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class CalorieCounterKtTest {
+    @Test
+    fun maxCaloriesExample() {
+        assertEquals(24000, sumTopNCalories("src/test/resources/day01-example.txt"))
+    }
+
+    @Test
+    fun maxCaloriesPuzzle() {
+        assertEquals(69289, sumTopNCalories("src/test/resources/day01-puzzle.txt"))
+    }
+
+    @Test
+    fun topNCaloriesSumExample() {
+        assertEquals(45000, sumTopNCalories("src/test/resources/day01-example.txt", topN = 3))
+    }
+
+    @Test
+    fun topNCaloriesSumPuzzle() {
+        assertEquals(205615, sumTopNCalories("src/test/resources/day01-puzzle.txt", topN = 3))
+    }
+}
