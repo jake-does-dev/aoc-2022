81236ab65c0d4d2c5824f3ed79a319ce1198be31
Jake Allan <jjemail1995@gmail.com>
2022-12-03 12:12:50 +0000

    day03 refactor

--- src/main/kotlin/day03/RucksackReorganisation.kt
+++ src/main/kotlin/day03/RucksackReorganisation.kt
@@ -2,61 +2,36 @@ package day03
 
 import utils.readFile
 
-fun sackSplitting(inputPath: String): Int {
-    return readLinesAsSequence(inputPath)
-        .map { it.splitting() }
-        .sumItemIntersectionPriorities()
-}
+val itemToPriority = ((('a'..'z') union ('A'..'Z')) zip ((1..26) union (27..52))).toMap()
 
-fun findBadges(inputPath: String): Int {
-    return readLinesAsSequence(inputPath)
+fun sackSplitting(inputPath: String): Int =
+    readLinesAsSequence(inputPath)
+        .map { it.splitAtMidpoint() }
+        .sumItemIntersectionPriorities()
+
+fun findBadges(inputPath: String): Int =
+    readLinesAsSequence(inputPath)
         .chunked(3)
         .sumItemIntersectionPriorities()
-}
 
-fun readLinesAsSequence(inputPath: String) : Sequence<String> =
+private fun readLinesAsSequence(inputPath: String): Sequence<String> =
     readFile(inputPath)
         .split("\n")
         .asSequence()
 
-fun String.splitting(): List<CharSequence> {
-    val midPoint = length / 2
-    return listOf(subSequence(0, midPoint), subSequence(midPoint, length))
-}
+private fun String.splitAtMidpoint(): List<CharSequence> =
+    listOf(subSequence(0, length / 2), subSequence(length / 2, length))
 
-private fun Sequence<List<CharSequence>>.sumItemIntersectionPriorities(): Int {
-    val itemToPriority = itemToPriority()
-
-    return this
-        .map { it.findItemIntersection() }
-        .flatten()
+private fun Sequence<List<CharSequence>>.sumItemIntersectionPriorities(): Int =
+    flatMap { it.findItemIntersection() }
         .map { itemToPriority[it] ?: 0 }
         .sum()
-}
 
-private fun List<CharSequence>.findItemIntersection(): Set<Char> {
-    return fold(get(0).items()) { items, next -> ((items intersect next.items()) as MutableSet<Char>) }
-}
+private fun List<CharSequence>.findItemIntersection(): Set<Char> =
+    fold(get(0).items()) { items, next -> ((items intersect next.items()) as MutableSet<Char>) }
 
 private fun CharSequence.items() =
-    this.itemToCount().keys
+    itemToCount().keys
 
-private fun CharSequence.itemToCount(): Map<Char, Int> {
-    val itemToCount = HashMap<Char, Int>()
-    this.forEach {
-        val previous = itemToCount.getOrDefault(it, 0)
-        itemToCount[it] = previous + 1
-    }
-    return itemToCount
-}
-
-private fun itemToPriority(): Map<Char, Int> {
-    val itemToPriority = HashMap<Char, Int>()
-
-    (('a'..'z') zip (1..26))
-        .forEach { (item, priority) -> itemToPriority[item] = priority }
-    (('A'..'Z') zip (27..52))
-        .forEach { (item, priority) -> itemToPriority[item] = priority }
-
-    return itemToPriority
-}
+private fun CharSequence.itemToCount(): Map<Char, Int> =
+    groupingBy { it }.eachCount()
