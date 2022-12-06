facc8164e579e84f0f5071e8af3c92e516b5a25f
jake-does-dev <jjemail1995@gmail.com>
2022-12-06 09:32:16 +0000

    day06

--- src/main/kotlin/day06/TuningTrouble.kt
+++ src/main/kotlin/day06/TuningTrouble.kt
@@ -1,28 +1,16 @@
 package day06
 
-import day06.Packet.MESSAGE
-import day06.Packet.SIGNAL
-
 fun findStartOf(inputPath: String, packet: Packet) : List<Int> =
     utils.readFile(inputPath)
         .split("\n")
         .map { it.findStartOf(packet) }
 
 private fun String.findStartOf(packet: Packet) : Int {
-    val windowSize = when (packet) {
-        SIGNAL -> 4
-        MESSAGE -> 14
-    }
-
-    val index = windowed(size = windowSize)
-        .withIndex()
-        .find { (_, packetData) -> packetData.toSet().size == windowSize }
-        ?.index ?: throw IllegalStateException("No start of packet found in signal!")
-
-    return index + windowSize
+    val packetSize = packet.size
+    return packetSize + windowed(packetSize).indexOfFirst { it.toSet().size == packetSize }
 }
 
-enum class Packet {
-    SIGNAL,
-    MESSAGE;
+enum class Packet(val size: Int) {
+    SIGNAL(4),
+    MESSAGE(14);
 }
